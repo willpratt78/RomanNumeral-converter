c2a3cb40a00149fb539d1accba22d9cfd84b3b7b
William Pratt <willpratt78@gmail.com>
2020-04-02 13:54:44 -0700

    fix bug by having two seperate functions and giving a base case'

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,24 @@
 var numberSet = [[1000, 'M'], [900,'CM'], [500, 'D'],  [400, 'CD'], [100, 'C'], 
 [90, 'XC'], [50, 'L'], [40, 'XL'], [10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'], [1,'I']];
 
-var romanNumber = function(number) {
+var numChecker = function(number) {
   if (number < 1 || number > 3999) {
-    return "This number isn't a roman numeral"
-  };
+    return false; //"This number isn't a roman numeral"
+  } else {
+    return true;
+  }
+};
+
+var romanNumber = function(number) {
+  if (number === 0) {
+    return "";
+  }
   for (var i = 0;  i < numberSet.length ; i++) {
     if (number >= numberSet[i][0]) {
       return numberSet[i][1] + romanNumber(number - numberSet[i][0]);
-    }
     };
   };
+};
 
 
 //UI logic
@@ -22,10 +30,14 @@ var romanNumber = function(number) {
 $(document).ready(function() {
   $("#converter").submit(function(event) {
     event.preventDefault();
-    // var romanNumber = number
     var userNumber = parseInt($("#number").val());
+    var result;
 
-    let result = romanNumber(userNumber);
+    if (numChecker(userNumber) === true) {
+      result = romanNumber(userNumber);
+    } else {
+      result = "This number can't be a roman numeral"
+    }
 
     $("#output").text(result);
     $(".well").show();
@@ -33,3 +45,6 @@ $(document).ready(function() {
 });
 
 // func() vs func = function(){}
+// https://www.learnhowtoprogram.com/react/functional-programming-with-javascript-3399822d-e51a-4550-8a09-b2e81cf0a43d/recursion-4e6c5262-4a01-448d-b06f-f1078856739e
+
+// https://www.youtube.com/watch?v=Qlr-FGbhKaI
